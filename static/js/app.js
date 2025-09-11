1a4cfea8df8c5acc34878952774f1159f497f1f7
Vishu <vishubhai9812@gmail.com>
2025-09-11 16:03:54 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,14 @@ new Vue({
         selectedSortKey: '',
         sortAsc: true,
         sortableKeys: ['id', 'param_name', 'param_theme', 'param_gen_frequency', 'aggregation_period', 'param_displayname'],
+        key_id: null,
+        paramRawData: [],
+        paramFilteredData: [],
+        paramLoading: false,
+        paramError: null,
+        paramSelectedSortKey: '',
+        paramSortAsc: true,
+        paramSortableKeys: ['param_id', 'param_name', 'geoentity_source_id', 'geoentity_name', 'param_theme'],
     },
     computed: {
         keyLabels() {
@@ -22,6 +30,14 @@ new Vue({
                 param_displayname: 'Param Display Name',
             };
         },
+        paramKeyLabels() {
+            return {
+                param_id: 'Param Id',
+                param_name: 'Param Name',
+                geoentity_source_id: 'Geoentity Source ID',
+                param_theme: 'Param Theme',
+            };
+        }
     },
     methods: {
         fetchData() {
@@ -84,14 +100,57 @@ new Vue({
                 this.sortBy(this.selectedSortKey, false);
             }
         },
-        toggleSortOrder() {
-            this.sortAsc = !this.sortAsc;
-            if (this.selectedSortKey) {
-                this.sortBy(this.selectedSortKey, false);
+        redirectToParam(id) {
+            window.location.href = `/list/key=${id}`;
+        },
+        fetchParamData() {
+            this.paramLoading = true;
+            this.paramError = null;
+            fetch(`http://192.168.2.202:5001/params-source-ids/${this.key_id}`)
+                .then(res => res.json())
+                .then(data => {
+                    this.paramRawData = data.data;
+                    this.paramFilteredData = this.paramRawData.slice();
+
+                    this.paramSelectedSortKey = 'geoentity_source_id';
+                    this.paramSortAsc = true;
+                    this.paramSortBy('geoentity_source_id', false);
+                })
+                .catch(() => {
+                    this.paramError = 'Failed to load parameter data';
+                })
+                .finally(() => {
+                    this.paramLoading = false;
+                });
+        },
+        paramSortBy(key, toggle = true) {
+            if (toggle) {
+                if (this.paramSelectedSortKey === key) {
+                    this.paramSortAsc = !this.paramSortAsc;
+                } else {
+                    this.paramSelectedSortKey = key;
+                    this.paramSortAsc = true;
+                }
             }
+
+            this.paramFilteredData.sort((a, b) => {
+                const valA = a[key] != null ? a[key] : '';
+                const valB = b[key] != null ? b[key] : '';
+                if (typeof valA === 'number' && typeof valB === 'number') {
+                    return this.paramSortAsc ? valA - valB : valB - valA;
+                }
+                return this.paramSortAsc
+                    ? String(valA).localeCompare(String(valB))
+                    : String(valB).localeCompare(String(valA));
+            });
+        },
+        goBack() {
+            window.history.back();
         }
     },
     mounted() {
         this.fetchData();
+        this.key_id = key_id; // Assign the variable key_id, make sure it is defined in the scope
+        this.fetchParamData();
     }
 });

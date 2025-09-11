dcbb3062ef81c6c80ec0173d0581a84db6e4d8a1
Vishu <vishubhai9812@gmail.com>
2025-09-11 16:03:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/list/key=<int:key_id>')
+def parameter_page(key_id):
+    return render_template('parameter_id.html', key_id=key_id)
+
+
+@app.route('/register/<int:key_id>')
+def register_parameter(key_id):
+    return f"Register parameter with key_id: {key_id}"
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5004)
